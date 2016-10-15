c05623e434d461bf958d3386aec9e6437154e0c7
chernaya <chern8904904@gmail.com>
2016-10-15 11:08:27 +0600

    логинпароль

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Created by user on 01.10.2016.
@@ -7,11 +8,21 @@ import java.util.List;
 public class Hello {
     public static void main(String[] args) {
         System.out.println(args[1]);
-        List<User> userList=new ArrayList<>();
+        String login = args[0];
+        String pass = args[1];
 
-        userList.add(new User(1,"ururur","123","new"));
-        userList.add(new User(2,"urur","123","new"));
-        System.out.println(p.login);
+        List<User> userList = new ArrayList<>();
+
+        userList.add(new User(1, "ururur", "123", "new"));
+        userList.add(new User(2, "urur", "123", "new"));
+        //System.out.println(p.login);
+        for (int i = 0; i < userList.size(); i++) {
+            final User user = userList.get(i);
+            if (user.login.equals(login)&&user.pass.equals(pass)) {
+                System.out.println("lalal");
+            }
+
+        }
         System.exit(1);
 
     }
