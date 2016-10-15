87d943ec6e7a0bdf5ae2bed2ac444218705d20ac
chernaya <chern8904904@gmail.com>
2016-10-15 10:56:25 +0600

    список

--- src/Hello.java
+++ src/Hello.java
@@ -1,10 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by user on 01.10.2016.
  */
 public class Hello {
     public static void main(String[] args) {
         System.out.println(args[1]);
-        User p = new User(1,"ururur","123","new");
+        List<User> userList=new ArrayList<>();
+
+        userList.add(new User(1,"ururur","123","new"));
+        userList.add(new User(2,"urur","123","new"));
         System.out.println(p.login);
         System.exit(1);
 
