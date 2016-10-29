3aa8f2337567f4ffad5fe7fb8f5dc15f79a2b611
chernaya <chern8904904@gmail.com>
2016-10-29 10:13:10 +0600

    test

---
+++ test.bat
@@ -0,0 +1,14 @@
+@echo off
+echo ***
+java -classpath "out/production/anyalena/" Hello yyy ee
+echo %errorlevel%
+echo ***
+java -classpath "out/production/anyalena/" Hello urur 123
+echo %errorlevel%
+echo ***
+java -classpath "out/production/anyalena/" Hello urur q
+echo %errorlevel%
+echo ***
+java -classpath "out/production/anyalena/" Hello
+echo %errorlevel%
+
