071c10e002d8f899ed2b724c5b7fd9efd41339ab
chernaya <chern8904904@gmail.com>
2016-10-29 09:53:51 +0600

    metod

--- src/Hello.java
+++ src/Hello.java
@@ -1,6 +1,5 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 
 public class Hello {
@@ -14,14 +13,35 @@ public class Hello {
         userList.add(new User(1, "ururur", "123", "new"));
         userList.add(new User(2, "urur", "123", "new"));
         //System.out.println(p.login);
+        /*
+        есть список пользователей с паролями.
+                какой-либо пользователь вводит свои данные(логин и проль).
+        нужно проверить их правдивость.
+        проверяем если такой логин?
+        если его нет то ошибка.
+                если есть то проверяем его пароль.
+                если нет то ошибка
+        если есть то
+
+                */
+        boolean result;
         for (int i = 0; i < userList.size(); i++) {
             final User user = userList.get(i);
-            if (user.login.equals(login)&&user.pass.equals(pass)) {
-                System.out.println("lalal");
+            if (user.login.equals(login)) {
+                if (user.pass.equals(pass)) {
+                    System.exit(0);
+                }
+                else
+                {
+                    System.exit(2);
+                }
             }
 
         }
+
         System.exit(1);
 
     }
+
+
 }
