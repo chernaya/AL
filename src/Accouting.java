6c0ca860575765d9ada4a8ec25542e53f4975d7e
chernaya <chern8904904@gmail.com>
2016-11-30 16:11:57 +0700

    Accounting(+)

---
+++ src/Accouting.java
@@ -0,0 +1,19 @@
+
+public class Accouting {
+    public int id;
+    public int role_id;
+    public int accounting;
+    public String date_st;
+    public String date_end;
+
+    public Accouting(int i, int r, int a, String ds, String de)
+    {
+        id=i;
+        role_id=r;
+        accounting=a;
+        date_st=ds;
+        date_end=de;
+    }
+
+
+}
